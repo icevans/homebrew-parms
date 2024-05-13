972b19f6a5b43fbfa822ca37e034e0aa0ff0c81d
Ian Evans <icevans@icloud.com>
2024-05-12 19:47:08 -0700

    update version to match

--- Formula/parms.rb
+++ Formula/parms.rb
@@ -5,7 +5,7 @@ class Parms < Formula
   homepage "https://github.com/icevans/parms"
   url "https://github.com/icevans/parms/releases/download/v1.0.0/parms"
   sha256 "45d41d4fb6362a67373d34d25c1d09a7e84ac1c027119c922aa14d06f88c825b"
-  version "0.1.0"
+  version "1.0.0"
 
   def install
     bin.install "parms"
