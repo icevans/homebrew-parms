b47bb0eabe267a11b3b488b92ab345a2692a40db
Ian Evans <icevans@icloud.com>
2024-05-12 19:51:09 -0700

    fix release download path

--- Formula/parms.rb
+++ Formula/parms.rb
@@ -3,7 +3,7 @@
 class Parms < Formula
   desc "Simple CLI to make interacting with SSM parameters not suck"
   homepage "https://github.com/icevans/parms"
-  url "https://github.com/icevans/parms/releases/download/v1.0.0/parms"
+  url "https://github.com/icevans/parms/releases/download/v1.0.0/parms-mac.tar.gz"
   sha256 "45d41d4fb6362a67373d34d25c1d09a7e84ac1c027119c922aa14d06f88c825b"
   version "1.0.0"
 
